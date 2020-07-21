b5189c7cd15f3234440f5e2c7e8ccd905a3c30eb
Giovanny Arias <giovannyarias@live.com>
2020-07-21 15:51:32 -0500

    :truck: - Delete old front

--- meli-app-front-old/src/helpers/getItems.js
+++
@@ -1,8 +0,0 @@
-export const getItems = async( search ) => {
-
-    const url = `http://localhost:4000/api/items?q=${ search }`;
-    const resp = await fetch( url );
-    const { data } = await resp.json();
-
-    return data;
-}
